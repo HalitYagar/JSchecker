fff4c6332d46ad075e0a6a7183d99d72175d347c
Halit Yagar <halityagar45@gmail.com>
2022-05-30 22:32:56 +0300

    background script updated so it can read and write cookies depend on the situtation

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -4,43 +4,43 @@ function getActiveTab() {
   return browser.tabs.query({active: true, currentWindow: true});
 }
 
-function handleMessage(request, sender, sendResponse) {
-  if(request.type == "set"){
-    sendResponse({response: "Cookies Set"});
-    cookieVal = request.cookieVal;
-    cookieStr = JSON.stringify(cookieVal)
-    const now = Date.now()/1000; // Unix timestamp in milliseconds
-    // console.log(cookieVal);
-    // console.log(cookieStr);
-    getActiveTab().then((tabs) => {
-    browser.cookies.set({
-      url: tabs[0].url,
-      name: "JSchecker",
-      value: cookieStr,
-      expirationDate: now + seconds_in_year
-    })
-    });
-  } else if(request.type == "get"){
-    sendResponse({response: "Getting Cookies"});
-    console.log("get the cookies");
-    getActiveTab().then((tabs) => {
-      // get any previously set cookie for the current tab
-      var gettingCookies = browser.cookies.get({
-        url: tabs[0].url,
-        name: "JSchecker"
-      });
-      gettingCookies.then((cookie) => {
-        if (cookie) {
-          var val = JSON.parse(cookie.value);
-          console.log(cookie);
-          console.log(val);
-        }
-      });
-    });
-  } else if(request.type == "check"){
-    console.log("check cookies");
+function compare_cokies(curr,old){
+  console.log(curr);
+  console.log(old);
 
-  }
+}
+
+function handleMessage(request, sender, sendResponse) {
+  console.log("get the cookies");
+  sendResponse({response: "Getting Cookies"});
+  currentCookie = request.cookieVal;
+  cookieStr = JSON.stringify(currentCookie)
+  const now = Date.now()/1000; // Unix timestamp in milliseconds
+
+  getActiveTab().then((tabs) => {
+    // get any previously set cookie for the current tab
+    var existingCookie;
+    var gettingCookies = browser.cookies.get({
+      url: tabs[0].url,
+      name: "JSchecker"
+    });
+    gettingCookies.then((cookie) => {
+      if (cookie) { // there is cookies that saved previously
+        existingCookie = JSON.parse(cookie.value);
+        // console.log(existingCookie);
+        console.log("compare cookies ");
+        compare_cokies(currentCookie,existingCookie);
+      } else {  // there are no currently set cookies
+        browser.cookies.set({
+          url: tabs[0].url,
+          name: "JSchecker",
+          value: cookieStr,
+          expirationDate: now + seconds_in_year
+        });
+        console.log("adding new cookie ");
+      }
+    });
+  });
 }
 
 browser.runtime.onMessage.addListener(handleMessage);

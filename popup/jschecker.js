0d84a8f710f253722060c41263a7126da762a673
Halit Yagar <halityagar45@gmail.com>
2022-06-03 01:12:56 +0300

    removed background script and merged to popup, updated popup functionality check and reset functions improved

--- popup/jschecker.js
+++ popup/jschecker.js
@@ -1,3 +1,7 @@
+const seconds_in_year = 60 * 60 * 24 * 365
+
+var checkText = document.querySelector('#checkText');
+var resetText = document.querySelector('#resetText');
 var check = document.querySelector('.check button');
 var reset = document.querySelector('.reset button');
 
@@ -6,9 +10,10 @@ function getActiveTab() {
 }
 
 function handleResponse(message) {
-    console.log(`Message from the background script:  ${message.response}`);
+    console.log(`Message from the update script:  ${message.response}`);
     let count = document.querySelector("#count");
     count.innerText = "JS files : "+message.response;
+
 }
 
 function handleError(error) {
@@ -33,6 +38,56 @@ reset.onclick = function() {
             name: "JSchecker"
           });
     });
-    console.log("cookie removed");
-
+    // console.log("cookie removed");
+    checkText.innerHTML = "";
+    resetText.innerHTML = "cookie removed";
 }
+
+function compare_cokies(curr,old){
+  currStr = JSON.stringify(curr);
+  oldStr = JSON.stringify(old);
+
+  if(currStr === oldStr){
+    console.log("cookies are same");
+    checkText.innerHTML = "&#9989;"+"Done";
+  } else {
+    console.log("cookies are different");
+    checkText.innerHTML = "Cookies are different";
+  }
+}
+
+function handleMessage(request, sender, sendResponse) {
+  console.log("get the cookies");
+  sendResponse({response: "Getting Cookies"});
+  currCookie = request.cookieVal;
+  cookieStr = JSON.stringify(currCookie)
+  const now = Date.now()/1000; // Unix timestamp in milliseconds
+
+  getActiveTab().then((tabs) => {
+    // get any previously set cookie for the current tab
+    var oldCookie;
+    var gettingCookies = browser.cookies.get({
+      url: tabs[0].url,
+      name: "JSchecker"
+    });
+    gettingCookies.then((cookie) => {
+      if (cookie) { // there is cookies that saved previously
+        oldCookie = JSON.parse(cookie.value);
+        // console.log(existingCookie);
+        console.log("compare cookies ");
+        compare_cokies(currCookie,oldCookie);
+      } else {  // there are no currently set cookies
+        checkText.innerHTML = "&#9989;"+"Cookie set";
+        browser.cookies.set({
+          url: tabs[0].url,
+          name: "JSchecker",
+          value: cookieStr,
+          expirationDate: now + seconds_in_year
+        });
+        console.log("adding new cookie ");
+      }
+    });
+  });
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
